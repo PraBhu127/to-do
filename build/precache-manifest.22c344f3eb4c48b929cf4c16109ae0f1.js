53c6f6ba77d0a37bdcc2644ec250083513ae5c1d
PraBhu127 <57143930+PraBhu127@users.noreply.github.com>
2020-06-30 13:52:58 +0545

    Add files via upload

---
+++ build/precache-manifest.22c344f3eb4c48b929cf4c16109ae0f1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1fcec770f1e2e778c480430f06f1000",
+    "url": "/to-do-app-react/index.html"
+  },
+  {
+    "revision": "c370df1442d09d4a96b4",
+    "url": "/to-do-app-react/static/css/main.4d0df6de.chunk.css"
+  },
+  {
+    "revision": "0e9e2dc2f3073b548650",
+    "url": "/to-do-app-react/static/js/2.816b83ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-app-react/static/js/2.816b83ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c370df1442d09d4a96b4",
+    "url": "/to-do-app-react/static/js/main.1a732bd6.chunk.js"
+  },
+  {
+    "revision": "36d75acc98e36834ed28",
+    "url": "/to-do-app-react/static/js/runtime-main.d463cb42.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-app-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
